f6b1987391c7fa999fdee2897581a402027fd1bf
studentofcoding <yonathanevan@gmail.com>
2019-08-12 18:28:24 +0700

    Hamburger dan Responsive Mobile Menu
    - Menambahkan Hamburger,
    - Mensetting Responsive Mobile Menu,
    - Mensetting Navlink untuk menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,22 @@ import './App.css';
 import { NavLink } from 'react-router-dom';
 
 import logo_pemimpin from '../src/image/Logo.png';
+import { Button, Dimmer, Menu } from 'semantic-ui-react';
 
 class Navbar extends Component {
+  state = {
+    activePage: 'home'
+  }
+
+  menuOpen = () => this.setState({ active: true })
+  menuClose = () => this.setState({ active: false })
+  handleItemClick = (e, { name }) => {
+    this.setState({ activePage: name })
+  }
 
   render() {
+    const { active, activePage } = this.state
+
     return (
       <header className="navigation-container_grid">
         <div className="logo-container">
@@ -17,38 +29,51 @@ class Navbar extends Component {
           </div>
         </div>
         {/*This is the function to Show the Menu Page*/}
-        {/* <div className="navigation-container">
-          <div className="menu_container">
-            <div className="navigation_item">About</div>
-            <div className="navigation_item">Term & Condition</div>
-            <div className="navigation_item">Contact</div>
-            <div className="navigation_item">Login</div>
-          </div>
-        </div> */}
           <div className="menu_container">
             <NavLink className="navigation_item" exact to="/about">About</NavLink>
             <NavLink className="navigation_item" exact to="/tnc">T&C</NavLink>
             <NavLink className="navigation_item" exact to="/contact">Contact</NavLink>
             <NavLink className="navigation_item" exact to="/login">Login</NavLink>
+            <div className="menu_button">
+            <Button icon='align justify' inverted color='red' onClick={this.menuOpen} />
+              <Dimmer active={active} onClickOutside={this.menuClose} page>
+                <Menu size='massive' inverted secondary vertical>
+                  <Menu.Item
+                    className="menu_item"
+                    as={NavLink}
+                    name='About'
+                    active={activePage === 'about'}
+                    exact to='/about'
+                  />
+                  <Menu.Item
+                    className="menu_item"
+                    as={NavLink}
+                    name='T&C'
+                    active={activePage === 'tnc'}
+                    onClick={this.handleItemClick}
+                    exact to='/tnc'
+                  />
+                  <Menu.Item
+                    className="menu_item"
+                    as={NavLink}
+                    name='Contact'
+                    active={activePage === 'contact'}
+                    onClick={this.handleItemClick}
+                    exact to='/contact'
+                  />
+                  <Menu.Item
+                    className="menu_item"
+                    as={NavLink}
+                    name='Login'
+                    active={activePage === 'login'}
+                    onClick={this.handleItemClick}
+                    exact to='/login'
+                  />
+                </Menu>
+              </Dimmer>
+            </div>
           </div>
       </header>
-
-    //   {/* Navigation Section */}
-    //   <header className="navigation-container_grid">
-    //   <div className="logo-container">
-    //     <div className="logo-container_container">
-    //       <img src={logo_pemimpin} alt="Pemimpin.co" className="app-logo"/>
-    //     </div>
-    //   </div>
-    //   <div className="navigation-container">
-    //     <div className="menu_container">
-    //       <div className="navigation_item">About</div>
-    //       <div className="navigation_item">Term & Condition</div>
-    //       <div className="navigation_item">Contact</div>
-    //       <div className="navigation_item">Login</div>
-    //     </div>
-    // //   </div>
-    // </header>
     );
   }
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-    endpoint: process.env.API_URL || 'http://localhost:3003'
+    endpoint: process.env.API_URL || 'http://fellowship.pemimpin.id:3000'
 }
 
 export default config;
